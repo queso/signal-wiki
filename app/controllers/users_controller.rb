dd2aa10e6f7268ccd0288f3aa5f1d1d27dd0993b
josh <josh@c8df4723-973c-463a-b466-deb924a697da>
2007-08-28 19:10:14 +0000

    Adding the user interface scaffolding.
    git-svn-id: svn://svn.roundhaus.com/steelpixel/signal/trunk@13 c8df4723-973c-463a-b466-deb924a697da

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  # Be sure to include AuthenticationSystem in Application Controller instead
+  include AuthenticatedSystem
+
+  # render new.rhtml
+  def new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    @user.save!
+    self.current_user = @user
+    redirect_back_or_default('/')
+    flash[:notice] = "Thanks for signing up!"
+  rescue ActiveRecord::RecordInvalid
+    render :action => 'new'
+  end
+
+end
