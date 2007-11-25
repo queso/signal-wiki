c42fa7d40b61e3e9a7d637aa7716100c29ec1c5f
josh <josh@c8df4723-973c-463a-b466-deb924a697da>
2007-11-25 04:27:47 +0000

    Added gravatar support to the user partial.  Also moved the /users link in the navbar to be public for everyone. [#19]
    git-svn-id: svn://svn.roundhaus.com/steelpixel/signal/trunk@78 c8df4723-973c-463a-b466-deb924a697da

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
     (user && user.login) ? user.login.to_s.capitalize : "Anonymous"
   end
   
+  def gravatar_url(email, size=70)
+    email ||= "nil@nil.com" 
+    require 'digest/md5'
+    digest = Digest::MD5.hexdigest(email)
+    "http://www.gravatar.com/avatar.php?size=#{size}&gravatar_id=#{digest}"
+  end
+  
 end
