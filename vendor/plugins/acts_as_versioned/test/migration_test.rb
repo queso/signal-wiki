1abc46fa78a0388c49d6d1d194e283a1d652788a
Codafoo <codafoo@gmail.com>
2008-04-14 02:17:59 -0700

    Using my acts_as_versioned which uses dirty objects

--- vendor/plugins/acts_as_versioned/test/migration_test.rb
+++ vendor/plugins/acts_as_versioned/test/migration_test.rb
@@ -21,8 +21,17 @@ if ActiveRecord::Base.connection.supports_migrations?
       assert_raises(ActiveRecord::StatementInvalid) { Thing.create :title => 'blah blah' }
       # take 'er up
       ActiveRecord::Migrator.up(File.dirname(__FILE__) + '/fixtures/migrations/')
-      t = Thing.create :title => 'blah blah'
+      t = Thing.create :title => 'blah blah', :price => 123.45, :type => 'Thing'
       assert_equal 1, t.versions.size
+      
+      # check that the price column has remembered its value correctly
+      assert_equal t.price,  t.versions.first.price
+      assert_equal t.title,  t.versions.first.title
+      assert_equal t[:type], t.versions.first[:type]
+      
+      # make sure that the precision of the price column has been preserved
+      assert_equal 7, Thing::Version.columns.find{|c| c.name == "price"}.precision
+      assert_equal 2, Thing::Version.columns.find{|c| c.name == "price"}.scale
 
       # now lets take 'er back down
       ActiveRecord::Migrator.down(File.dirname(__FILE__) + '/fixtures/migrations/')
